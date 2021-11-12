f9ae7bba902cfa4bd7fc2487d3c754e418ad5c86
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-11-12 16:47:35 +0000

    scrape theory list

--- scraper.js
+++ scraper.js
@@ -1,15 +1,17 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const { parse } = require("path");
 
 (async () => {
   const [browser, page] = await initBrowserPage(
     "https://changingminds.org/explanations/theories/theories.htm"
   );
 
-  const disciplines = await scrapeDisciplines(page);
+  const root = await scrapeDisciplines(page);
+  await scrapeTheories(page, root);
 
-  const result = disciplines;
-  writeResults(result);
+  writeResults(root);
+
   await browser.close();
 })();
 
@@ -23,18 +25,44 @@ async function initBrowserPage(pageUrl) {
   return [browser, page];
 }
 
+/**
+ * @param {puppeteer.Page} page
+ */
 async function scrapeDisciplines(page) {
   console.log("Scraping theory disciplines...");
-  const disciplines = await page.$$eval("li", (listItems) => {
+  const lists = await page.$$("ul");
+  const disciplineListHandle = lists[1];
+  return await parseList(disciplineListHandle);
+}
+
+/**
+ * @param {puppeteer.Page} page
+ */
+async function scrapeTheories(page, disciplines) {
+  for (let i = 0; i < disciplines.length; i++) {
+    const discipline = disciplines[i];
+    await page.goto(discipline.url);
+    console.log(`scraping ${discipline.url}...`);
+    const theoryListHandle = await page.$("ul");
+    discipline.theories = await parseList(theoryListHandle);
+  }
+}
+/**
+ *
+ * @param {puppeteer.ElementHandle} listHandle
+ */
+async function parseList(listHandle) {
+  const list = await listHandle.$$eval("li", (listItems) => {
     return listItems.map((listItem) => ({
       title: listItem.querySelector("a").innerHTML.trim(),
       url: listItem.querySelector("a").href,
-      description: listItem.innerHTML.match(/:(.+)/)[1].trim(),
+      description: listItem.innerHTML.match(/:(.+)/s)?.[1].trim(),
     }));
   });
-  return disciplines;
+  return list;
 }
 
 function writeResults(data) {
+  console.log("writing results...");
   fs.writeFileSync("scraped.json", JSON.stringify(data));
 }
