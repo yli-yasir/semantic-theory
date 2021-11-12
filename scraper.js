3231d5a089fc5151ad2f16bfbddad5d2548b1fdf
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-11-12 01:46:46 +0000

    use funcs

--- scraper.js
+++ scraper.js
@@ -1,24 +1,40 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
+  const [browser, page] = await initBrowserPage(
+    "https://changingminds.org/explanations/theories/theories.htm"
+  );
+
+  const disciplines = await scrapeDisciplines(page);
+
+  const result = disciplines;
+  writeResults(result);
+  await browser.close();
+})();
+
+async function initBrowserPage(pageUrl) {
   console.log("Starting browser!");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   console.log("Going to page!");
-  await page.goto(
-    "https://changingminds.org/explanations/theories/theories.htm"
-  );
+  await page.goto(pageUrl);
+  return [browser, page];
+}
 
+async function scrapeDisciplines(page) {
   console.log("Scraping theory disciplines...");
-  const theories = await page.$$eval("li", (listItems) => {
+  const disciplines = await page.$$eval("li", (listItems) => {
     return listItems.map((listItem) => ({
       title: listItem.querySelector("a").innerHTML.trim(),
+      url: listItem.querySelector("a").href,
       description: listItem.innerHTML.match(/:(.+)/)[1].trim(),
     }));
   });
+  return disciplines;
+}
 
-  console.log("Result:");
-  console.log(theories);
-  await browser.close();
-})();
+function writeResults(data) {
+  fs.writeFileSync("scraped.json", JSON.stringify(data));
+}
